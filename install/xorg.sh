0eb732bb76e4b2c13620df04eee1bb43525542ab
Erick Chacon <eral.th07@gmail.com>
2022-12-07 22:56:13 -0500

    xorg

--- install/xorg.sh
+++ install/xorg.sh
@@ -1,17 +1,3 @@
 # xorg
 sudo pacman -S xorg-server xorg-xinit xorg
 
-# st
-git clone https://github.com/LukeSmithxyz/st
-cd st
-sudo make install
-cd ..
-
-# dmenu
-sudo pacman -S dmenu
-
-# dwm
-# git clone https://github.com/ErickChacon/dwm.git
-# git checkout patched
-# sudo make clean install
-
