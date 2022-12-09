f82c163861be0f2374761594b4d0d40e7f8b51f7
Erick Chacon <eral.th07@gmail.com>
2022-12-09 17:54:36 -0500

    neovim

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,4 +1,6 @@
+# python provider
+sudo pacman -S python-pynvim
+
+# packer plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# ~/.config/nvim/lua/plugins.lua
