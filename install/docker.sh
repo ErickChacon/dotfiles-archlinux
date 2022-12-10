7ad1f96433883d6135b8982219725a06caf3a65c
Erick Chacon <eral.th07@gmail.com>
2022-12-09 20:03:14 -0500

    docker

--- install/docker.sh
+++ install/docker.sh
@@ -1,16 +1,23 @@
-# docker
-pacman -S docker docker-compose
+# install docker with cache space to /home/docker
+
+sudo pacman -S docker docker-compose
+sudo mkdir -p /home/docker
+sudo echo "{\n\t\"data-root\": \"/home/docker\"\n}" > /etc/docker/daemon.json
 sudo usermod -a -G docker $USER
 systemctl enable --now docker.service
-# restart
+docker info
 
-# https://www.ibm.com/docs/en/z-logdata-analytics/5.1.0?topic=compose-relocating-docker-root-directory
-# sudo systemctl stop docker
-sudo systemctl stop docker.service
-sudo systemctl stop docker.socket
-sudo mkdir -p /home/docker
-sudo nvim /etc/docker/daemon.json
-# {
-# 	"data-root": "/home/docker"
-# }
-sudo systemctl start docker
+# sudo nvim /etc/docker/daemon.json
+# # {
+# # 	"data-root": "/home/docker"
+# # }
+#
+#
+# # restart
+#
+# # # https://www.ibm.com/docs/en/z-logdata-analytics/5.1.0?topic=compose-relocating-docker-root-directory
+# # # sudo systemctl stop docker
+# # sudo systemctl stop docker.service
+# # sudo systemctl stop docker.socket
+# # sudo mkdir -p /home/docker
+# # sudo systemctl start docker
