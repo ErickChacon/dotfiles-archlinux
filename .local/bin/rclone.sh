a7e02e273b2c08b9fb197278dbbb215d191714e2
Erick <eral.th07@gmail.com>
2022-12-15 10:45:23 -0500

    gromit

--- .local/bin/rclone.sh
+++ .local/bin/rclone.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
+# rclone sync \
+#     ~/documents lancaster:documents --exclude .git/ --progress --retries 1
+
+# rclone sync \
+#     ~/documents/organizer lancaster:documents/organizer \
+#     --exclude personal/no.md  --progress --retries 1
+
+# rclone sync \
+#     ~/documents/organizer/personal lancaster:documents/organizer/personal \
+#     --exclude personal-information.md  --progress --retries 1
+
 rclone sync \
-    ~/documents lancaster:documents --exclude .git/ --progress --retries 1
+    ~/documents lancaster:documents \
+    --exclude /organizer/personal/personal-information.md \
+    --exclude .git/ --progress --retries 5
+
+
+
+
+
+
 
 # rclone sync \
 #     lancaster:archive ~/archive --progress
---
+++ .local/bin/xp-pen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/lib/pentablet/pentablet.sh /mini &
