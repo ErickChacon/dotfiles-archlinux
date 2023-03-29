127bf04b1bb828fe783f44e1f5484f35bbddd2ca
Erick <eral.th07@gmail.com>
2023-03-28 20:20:24 -0500

    hibernate

---
+++ install/hibernate.sh
@@ -0,0 +1,18 @@
+
+# add resume to the mkinitcpio hooks
+nvim /etc/mkinitcpio.conf
+# HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems resume fsck)
+mkinitcpio -p linux
+
+# set up grub for encrypted lvm partition
+nvim /etc/default/grub
+# GRUB_CMDLINE_LINUX="cryptdevice=UUID=myuuid:cryptlvm root=/dev/MyVolGroup/root resume=/dev/MyVolGroup/swap"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# hibernate when inactive
+nvim /etc/systemd/logind.conf
+#IdleAction=suspend
+#IdleActionSec=10min
+
+
+
