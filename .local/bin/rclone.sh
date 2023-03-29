e558d9121483d6233c570d3bf50740942317a7f1
Erick <eral.th07@gmail.com>
2023-03-28 20:21:10 -0500

    rclone

--- .local/bin/rclone.sh
+++ .local/bin/rclone.sh
@@ -2,7 +2,7 @@
 
 rclone sync \
     ~/documents uni:documents \
-    --exlude personal-information.md \
+    --exclude /organizer/personal/personal-information.md \
     --exclude .git/ --progress --retries 10
 
 # rclone sync \
@@ -13,20 +13,7 @@ rclone sync \
 #     --exclude personal/no.md  --progress --retries 1
 
 # rclone sync \
-#     ~/documents/organizer/personal lancaster:documents/organizer/personal \
-#     --exclude personal-information.md  --progress --retries 1
-
-# rclone sync \
-#     ~/documents lancaster:documents \
-#     --exclude /organizer/personal/personal-information.md \
-#     --exclude .git/ --progress --retries 10
-
-# rclone sync \
 #     lancaster:documents/teaching/uni-postgraduate/02-bayesian-statistics \
 #     ~/documents/teaching/uni-postgraduate/02-bayesian-statistics \
 #     --exclude .git/ --progress --retries 5
-
-
-# rclone sync \
-#     lancaster:archive ~/archive --progress
 
