8f626c8a821c6335b2ac0e62bacc7e2e79125302
Erick <eral.th07@gmail.com>
2023-03-29 13:15:45 -0500

    neovim

--- install/neovim.sh
+++ install/neovim.sh
@@ -4,3 +4,7 @@ sudo pacman -S python-pynvim xclip
 # packer plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Run on nvim
+# :PackerInstall
+# :PackerCompile
