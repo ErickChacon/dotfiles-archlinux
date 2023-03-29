65f010e3fc14fbb91a98883d84b68c7a051a6f08
Erick <eral.th07@gmail.com>
2023-03-29 13:16:28 -0500

    telescope dependencies

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,6 +1,9 @@
 # python provider
 sudo pacman -S python-pynvim xclip
 
+# telescope pre-requisites
+sudo pacman -S ripgrep fd
+
 # packer plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
