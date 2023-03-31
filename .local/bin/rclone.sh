cc88d9adea39e4606705ea4e98f006b442985bca
Erick <eral.th07@gmail.com>
2023-03-30 20:27:49 -0500

    rclone

--- .local/bin/rclone.sh
+++ .local/bin/rclone.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 
 rclone sync \
-    ~/documents uni:documents \
-    --exclude /organizer/personal/personal-information.md \
+    ~/documents/organizer uni:documents/organizer \
+    --exclude /personal/personal-information.md \
     --exclude .git/ --progress --retries 10
+
+rclone sync \
+    ~/documents/teaching uni:documents/teaching \
+    --exclude .git/ --progress --retries 10
+
+rclone sync \
+    ~/documents/projects/change-of-support \
+    uni:documents/projects/change-of-support \
+    --exclude .git/ --progress --retries 10
+
+rclone sync \
+    ~/documents/repositories/dotfiles-archlinux \
+    uni:documents/repositories/dotfiles-archlinux \
+    --exclude .git/ --progress --retries 10
+
+rclone sync \
+    ~/documents/repositories/toolbox \
+    uni:documents/repositories/toolbox \
+    --exclude .git/ --progress --retries 10
+
+
+
+
+# rclone sync \
+#     ~/documents uni:documents \
+#     --exclude /organizer/personal/personal-information.md \
+#     --exclude .git/ --progress --retries 10
 
 # rclone sync \
 #     ~/documents lancaster:documents --exclude .git/ --progress --retries 1
