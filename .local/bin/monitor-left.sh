5a66446676e868ad52a71c1e3b53bf7b8a89975a
Erick <eral.th07@gmail.com>
2023-10-04 09:42:49 +0300

    update behaviour of second monitor

--- .local/bin/monitor-left.sh
+++ .local/bin/monitor-left.sh
@@ -3,5 +3,11 @@
 # xrandr --output HDMI-A-1-0 --auto --left-of eDP
 # xrandr --output HDMI-1 --auto --left-of eDP-1
 # xrandr --auto --output HDMI-1 --mode 3840x2160 --left-of eDP-1
-xrandr --auto --output eDP-1 --off  --output HDMI-1 --mode 3840x2160 --primary
 # xrandr --auto --output HDMI-1 --off  --output eDP-1 --primary
+
+# # dell inspiron
+# xrandr --auto --output eDP-1 --off  --output HDMI-1 --mode 3840x2160 --primary
+
+# dell precision
+# xrandr --auto --output eDP-2 --off  --output HDMI-1-1 --mode 3840x2160 --primary
+xrandr --auto --output eDP-1 --off --output HDMI-1-0 --mode 3840x2160 --primary
